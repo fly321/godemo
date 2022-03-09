5429477e88ab242a2974295449d80ec494b6287b
aimer <15267840622@163.com>
2022-03-09 23:38:15 +0800

    string 操作

--- stringdemo/demo.go
+++ stringdemo/demo.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	path := "d:\\ff\\cc\\1.txt"
@@ -21,5 +24,26 @@ func main() {
 	ss1 := fmt.Sprintf("%s%s", s, s3)
 	fmt.Println(ss1)
 	fmt.Printf("%s%s", s, s3)
+
+	s4 := "我就在\\呼吸"
+
+	// 分割
+	ret := strings.Split(s4, "\\")
+	fmt.Println(ret)
+
+	// 包含
+	fmt.Println(strings.Contains(s4, "huhu"))
+
+	// 前缀
+	fmt.Println(strings.HasPrefix(s4, "我"))
+
+	// 后缀
+	fmt.Println(strings.HasSuffix(s4, "我"))
+
+	s5 := "abcdeb"
+	fmt.Println(strings.Index(s5, "c"))
+	fmt.Println(strings.LastIndex(s5, "b"))
+
+	fmt.Println(strings.Join(ret, "+"))
 
 }
