b5be7a5b1894bcf3d02bffd6c9a5b544ea131c62
aimer <15267840622@163.com>
2022-03-22 22:12:29 +0800

    goto跳出循环

---
+++ 03goto/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		for j := 0; j <= 10; j++ {
+			if j == 2 {
+				goto gotoTag
+			}
+			fmt.Println(i, j)
+		}
+	}
+	/**
+	0 0
+	0 1
+	xxx
+	*/
+	return
+
+gotoTag:
+	fmt.Println("xxx")
+}
