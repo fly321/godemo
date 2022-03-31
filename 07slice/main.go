275f4b9bfb3d7a85b6cc5376b203f7d6f5a9c325
aimer <15267840622@163.com>
2022-04-01 00:08:47 +0800

    数组切片

--- 07slice/main.go
+++ 07slice/main.go
@@ -19,10 +19,28 @@ func main() {
 	fmt.Println(s2 == nil) // false
 
 	// 长度和容量
-	fmt.Println("s1:len:%d,cap:%d", len(s1), cap(s1))
-	fmt.Println("s2:len:%d,cap:%d", len(s2), cap(s2))
+	fmt.Printf("s1:len:%d,cap:%d", len(s1), cap(s1))
+	fmt.Printf("s2:len:%d,cap:%d", len(s2), cap(s2))
 
 	// 由数组得到切片
 	a2 := [...]int{1, 2, 3, 4, 5}
 	fmt.Println(a2[0:2]) // 左包含右不包含
+
+	a3 := a2[:]
+	a4 := a2[1:]
+	a5 := a2[:2]
+
+	fmt.Println(a3)
+	fmt.Println(a4)
+	fmt.Println(a5)
+	fmt.Printf("a5:len:%d,cap:%d", len(a5), cap(a5))
+
+	/**
+	s1:len:3,cap:3s2:len:3,cap:3[1 2]
+	[1 2 3 4 5]
+	[2 3 4 5]
+	[1 2]
+	a5:len:2,cap:5
+	*/
+
 }
