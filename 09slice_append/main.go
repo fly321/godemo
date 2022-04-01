e24a9a2015f7cbbaa268b6637c834592ab81a32c
aimer <15267840622@163.com>
2022-04-01 23:59:39 +0800

    追加数组

--- 09slice_append/main.go
+++ 09slice_append/main.go
@@ -4,9 +4,19 @@ import "fmt"
 
 func main() {
 	// 追加数组
-	s2 := []string{"笑哈哈", "哈哈度"}
+	s2 := []string{"笑哈哈", "哈哈度", "对嘟嘟"}
+	fmt.Printf("s2:%v,len:%d,cap:%d\r\n", s2, len(s2), cap(s2))
 	// s2[] = “jhaja" 错误写法
 	s2 = append(s2, "叽里呱啦")
+	fmt.Printf("s2:%v,len:%d,cap:%d\r\n", s2, len(s2), cap(s2))
 
-	fmt.Println(s2)
+	s2 = append(s2, "叽里1")
+	fmt.Printf("s2:%v,len:%d,cap:%d\r\n", s2, len(s2), cap(s2))
+
+	/**
+	s2:[笑哈哈 哈哈度 对嘟嘟],len:3,cap:3
+	s2:[笑哈哈 哈哈度 对嘟嘟 叽里呱啦],len:4,cap:6
+	s2:[笑哈哈 哈哈度 对嘟嘟 叽里呱啦 叽里1],len:5,cap:6
+	*/
+
 }
