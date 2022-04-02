1921fe609a765426b3052898c8ae92ee69c16afa
aimer <15267840622@163.com>
2022-04-02 23:29:25 +0800

    copy

---
+++ 09slice_append/copy.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a1 := []int{1, 3, 5}
+	a2 := a1 //引用的
+
+	var a3 = make([]int, 3, 5)
+	copy(a3, a1) // 复制的
+
+	fmt.Println(a1, a2, a3)
+	a1[0] = 100
+	fmt.Println(a1, a2, a3)
+
+	// 将a1中的3移除
+	a1 = append(a1[:1], a1[2:]...)
+	fmt.Println(a1)
+
+}
