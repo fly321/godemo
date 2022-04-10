5b37942d8b1a45f82c367f3982275d070171a568
aimer <15267840622@163.com>
2022-04-11 00:00:38 +0800

    func

--- 17func/main.go
+++ 17func/main.go
@@ -4,6 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println(sumxy(1, 2))
+	f3()
+	fmt.Println(f4())
+}
+
+/**
+沒有返回值
+*/
+func f3() {
+	fmt.Println("f3")
+}
+
+/**
+返回一个int类型的值
+*/
+func f4() int {
+	return 4
 }
 
 func sumxy(x int, y int) (ret int) {
