0a26ab1aafd2eddbfce4a3a22e6ff1bce60d8a51
aimer <15267840622@163.com>
2022-04-16 23:12:03 +0800

    函数传参

---
+++ 17func/main1.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func f1() {
+	fmt.Println("嘻嘻")
+}
+
+func f2() int {
+	return 10
+}
+
+func f33(x func() int) int {
+	a := x()
+	return a
+}
+
+func main() {
+	a := f1
+	fmt.Printf("%T\r\n", a)
+	b := f2
+	fmt.Printf("%T\r\n", b)
+	c := f33(f2)
+	fmt.Println(c)
+
+}
