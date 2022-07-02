e3382dbacae49e74a985de97acd6473a0bb8242e
aimer <15267840622@163.com>
2022-07-03 00:41:39 +0800

    修改字符串 中文要用rune

--- stringdemo/demo2.go
+++ stringdemo/demo2.go
@@ -7,4 +7,11 @@ func main() {
 	byteStr := []byte(s1)
 	byteStr[0] = 'P'
 	fmt.Println(string(byteStr))
+
+	// 字符串有汉子转换
+	s2 := "中国"
+	runeStr := []rune(s2)
+	runeStr[0] = '大'
+	fmt.Println(string(runeStr))
+
 }
