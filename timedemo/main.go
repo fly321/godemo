1bd61c7a18cfbd171c51e389436e29126e206971
aimer <15267840622@163.com>
2022-07-13 23:16:37 +0800

    时间

--- timedemo/main.go
+++ timedemo/main.go
@@ -16,4 +16,8 @@ func main() {
 	minute := now.Minute()
 	second := now.Second()
 	fmt.Printf("%d-%02d-%02d %d:%d:%d\n", year, month, day, hour, minute, second)
+	// 2006 表示年份的格式 01 表示 月 02 表示日 03 表示时 04 表示分 05 表示秒
+	format := now.Format("2006-01-02 03:04:05")
+	fmt.Println(format)
+
 }
