beeae806448c9eed2fa5baf588a5a13d770553e4
fly3210 <15267840622@163.com>
2022-08-10 23:47:15 +0800

    解决互斥锁

--- chaneldemo/huchisuo.go
+++ chaneldemo/huchisuo.go
@@ -9,8 +9,13 @@ import (
 var count int = 0
 var wg1 sync.WaitGroup
 
+// 声明互斥锁
+var mutex sync.Mutex
+
 func test() {
+	mutex.Lock()
 	defer wg1.Done()
+	defer mutex.Unlock()
 	count++
 	fmt.Println("the count is : ", count)
 	time.Sleep(time.Millisecond)
