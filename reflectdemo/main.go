370d5cef4d302a7447b2542c71f4e7acc7cbfe11
fly3210 <15267840622@163.com>
2022-08-12 23:38:51 +0800

    反射修改变量的值

--- reflectdemo/main.go
+++ reflectdemo/main.go
@@ -5,10 +5,16 @@ import (
 	"reflect"
 )
 
+type myInt int
+type n1 struct {
+	Name string
+	Age  int
+}
+
 // 通过反射获取变量类型
 func reflectFn(x interface{}) {
 	of := reflect.TypeOf(x)
-	fmt.Println(of)
+	fmt.Printf("类型:%v,名称：%v,种类：%v\r\n", of, of.Name(), of.Kind())
 }
 
 func main() {
@@ -16,4 +22,21 @@ func main() {
 	reflectFn(a)
 	b := "3333"
 	reflectFn(b)
+
+	var m myInt = 1
+	reflectFn(m)
+
+	m1 := n1{
+		Name: "xixi",
+		Age:  11,
+	}
+	reflectFn(m1)
+
+	reflectFn(&m)
+
+	m2 := [3]int{1, 2, 3}
+	m3 := []int{11, 22, 33}
+	reflectFn(m2)
+	reflectFn(m3)
+
 }
