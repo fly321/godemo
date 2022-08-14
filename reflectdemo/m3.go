386aab8d3eec2354d0bb3c445b8d1ba631b9079d
fly3210 <15267840622@163.com>
2022-08-14 18:02:22 +0800

    调用传参方法,非传参方法

--- reflectdemo/m3.go
+++ reflectdemo/m3.go
@@ -11,7 +11,7 @@ type Student1 struct {
 	Score int    `json:"score"`
 }
 
-func (s *Student1) GetInfo() string {
+func (s Student1) GetInfo() string {
 	str := fmt.Sprintf("姓名：%v,年龄:%v,积分:%v\n", s.Name, s.Age, s.Score)
 	return str
 }
@@ -24,6 +24,35 @@ func (s *Student1) SetInfo(name string, age, score int) {
 
 func (s Student1) Print() {
 	fmt.Println("这是一个打印方法")
+}
+
+func PrintStructField1(s interface{}) {
+	t := reflect.TypeOf(s)
+	v := reflect.ValueOf(s)
+
+	if t.Kind() != reflect.Struct && t.Elem().Kind() != reflect.Struct {
+		fmt.Println("传入的参数不是一个结构体")
+		return
+	}
+
+	field0 := t.Field(0)
+	fmt.Printf("%#v\n", field0)
+
+	fmt.Printf("字段名称：%v\n", field0.Name)
+	fmt.Printf("字段类型：%v\n", field0.Type)
+	fmt.Printf("字段tag:%v\n", field0.Tag.Get("json"))
+
+	field1, ok := t.FieldByName("Age")
+	if ok {
+		fmt.Printf("字段名称：%v\n", field1.Name)
+		fmt.Printf("字段类型：%v\n", field1.Type)
+		fmt.Printf("字段tag:%v\n", field1.Tag.Get("json"))
+	}
+
+	// 调用方法
+	info := v.MethodByName("GetInfo").Call(nil)
+	fmt.Println(info)
+
 }
 
 func PrintStructField(s interface{}) {
@@ -72,5 +101,6 @@ func main() {
 		11,
 		12,
 	}
+	PrintStructField1(s1)
 	PrintStructField(&s1)
 }
