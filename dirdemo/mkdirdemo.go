9f2205c1b4628a4a3f4ae22ddec5f47700a2f853
fly3210 <15267840622@163.com>
2022-08-16 22:42:39 +0800

    创建目录删除目录

--- dirdemo/mkdirdemo.go
+++ dirdemo/mkdirdemo.go
@@ -6,5 +6,9 @@ func main() {
 	// 单层创建目录
 	os.Mkdir("./abc", 0777)
 	// 多曾创建目录
-	os.MkdirAll("./abc/def", 0777)
+	//os.MkdirAll("./abc/def", 0777)
+
+	//os.Remove("./abc")
+	//os.RemoveAll("./abc/def")
+	os.RemoveAll("./abc")
 }
