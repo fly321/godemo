37d339c0b26e4a675d7bf4df011fbd32cd55e414
fly3210 <15267840622@163.com>
2022-08-16 22:46:21 +0800

    修改文件名

--- dirdemo/mkdirdemo.go
+++ dirdemo/mkdirdemo.go
@@ -4,11 +4,13 @@ import "os"
 
 func main() {
 	// 单层创建目录
-	os.Mkdir("./abc", 0777)
+	//os.Mkdir("./abc", 0777)
 	// 多曾创建目录
 	//os.MkdirAll("./abc/def", 0777)
 
 	//os.Remove("./abc")
 	//os.RemoveAll("./abc/def")
-	os.RemoveAll("./abc")
+	//os.RemoveAll("./abc")
+
+	os.Rename("./1.txt", "./2.txt")
 }
